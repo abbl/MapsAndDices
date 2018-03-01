eeb5d6b9ab883f1e06a71ef28ea1d7661c000650
Patrick <bleowy1@gmail.com>
2018-03-02 00:17:05 +0100

    Switched engine to unity3D, implemented hex generation, added camera move/scroll.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+    private GameObject mainCamera;
+    public float cameraSpeed;
+    public float cameraScrollSpeed;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.Find("Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCamera();
+        ChangeCameraSize();
+	}
+
+    private void MoveCamera()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal * cameraSpeed, vertical * cameraSpeed, 0);
+        mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + movement.x, mainCamera.transform.position.y + movement.y, 0);
+    }
+
+    private void ChangeCameraSize()
+    {
+        Camera camera = mainCamera.GetComponentInChildren<Camera>();
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            camera.orthographicSize += 1 * cameraScrollSpeed;
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            camera.orthographicSize -= 1 * cameraScrollSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/HexagonMapGenerator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexagonMapGenerator : MonoBehaviour {
+    public GameObject sampleHexagon;
+    public int rows;
+    public int columns;
+    private ArrayList hexagons;
+
+	// Use this for initialization
+	void Start () {
+        hexagons = new ArrayList();
+        GenerateWorld(rows, columns);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void GenerateWorld(int rows, int colums)
+    {
+        for(int row = 1; row < rows; row++)
+        {
+            for(int column = 1; column < colums; column++)
+            {
+                createHexagon(column, row);
+            }
+        }
+    }
+
+    private GameObject createHexagon(int x, int y)
+    {
+        GameObject gameObject = Instantiate(sampleHexagon);
+        gameObject.transform.parent = GameObject.Find("HexagonMap").transform;
+        SetFixedPosition(gameObject, x, y);
+        return null;
+    }
+
+    private void SetFixedPosition(GameObject hexagon, int column, int row)
+    {
+        Vector2 positionVector = hexagon.transform.position;
+        float offset = 0;
+        if(column % 2 == 0)
+        {
+            offset = (getHexagonHeight() / 2);
+        }
+        positionVector.x = column * (getHexagonWidth() / 1.25f);
+        positionVector.y = (row * getHexagonHeight() * 1.05f) + offset;
+        hexagon.transform.position = positionVector;
+    }
+
+    private float getHexagonWidth()
+    {
+        return sampleHexagon.GetComponent<Renderer>().bounds.size.x;
+    }
+
+    private float getHexagonHeight()
+    {
+        return sampleHexagon.GetComponent<Renderer>().bounds.size.y;
+    }
+}
