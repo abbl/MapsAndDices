bb258171142c6f285fa6c382f3e74961ca27128b
Patrick <bleowy1@gmail.com>
2018-03-31 13:19:51 +0200

    Working on hexagon interaction.

---
+++ Assets/Scripts/Map/Hexagon/HexagonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HexagonController : MonoBehaviour {
+    private Hexagon hexagon;
+    private GameObject hexagonCard;
+
+    private void Start()
+    {
+        hexagon = GetComponent<Hexagon>();
+    }
+
+    public void DisplayHexagonCard()
+    {
+        CreateHexagonCard();
+    }
+
+    private void CreateHexagonCard()
+    {
+        hexagonCard = Resources.Load("Cards/DefaultHexCard") as GameObject;
+        Image hexImage = (Image)hexagonCard.transform.Find("HexImage").gameObject.GetComponent<Image>();
+        Text hexName = (Text)hexagonCard.transform.Find("HexName").gameObject.GetComponent<Text>();
+        Text hexDesc = (Text)hexagonCard.transform.Find("HexDesc").gameObject.GetComponent<Text>();
+        hexImage.sprite = hexagon.hexagonCardSprite;
+        hexName.text = hexagon.hexagonName;
+        hexDesc.text = hexagon.hexagonDesc;
+    }
+
+    public void HideHexagonCard()
+    {
+        if (hexagonCard != null)
+            Destroy(hexagonCard);
+    }
+}
---
+++ Assets/Scripts/Map/Hexagon/HexagonView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonView : MonoBehaviour {
+    private HexagonController hexagonController;
+
+	// Use this for initialization
+	void Start () {
+        hexagonController = GetComponent<HexagonController>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isHexagonLeftClicked();
+        isHexagonRightClicked();
+    }
+
+    /// <summary>
+    /// In case of left clicking player will move to certain hex
+    /// </summary>
+    private void isHexagonLeftClicked()
+    {
+
+    }
+
+    /// <summary>
+    /// In case of right clicking a hexagon description card will appear.
+    /// </summary>
+    private void isHexagonRightClicked()
+    {
+        hexagonController.DisplayHexagonCard();
+    }
+}
