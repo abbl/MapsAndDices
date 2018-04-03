85cf7c30f229788eb7f8eb000f2f5e03248c3ea0
Patrick <bleowy1@gmail.com>
2018-04-03 14:03:11 +0200

    Implemented custom NetworkManagerHUD

--- Assets/Scripts/UI/NetworkManagerGUI.cs
+++ Assets/Scripts/UI/NetworkManagerGUI.cs
@@ -7,16 +7,92 @@ using System.Collections;
 public class NetworkManagerGUI : MonoBehaviour
 {
     private NetworkManager networkManager;
+    public Text networkAddressField;
     public Canvas networkManagerCanvas;
+    public Canvas inGameCanvas;
 
     private void Awake()
     {
         networkManager = GetComponent<NetworkManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        networkManagerCanvas.gameObject.SetActive(true);
 
+        if (inGameCanvas != null)
+            inGameCanvas.gameObject.SetActive(false);
+    }
+
+    public void Host()
+    {
+        if (IsAnyNetworkServiceStarted())
+        {
+            networkManager.StartHost();
+        }
+    }
+
+    public void Connect()
+    {
+        if (IsAnyNetworkServiceStarted())
+        {
+            SetNetworkAddress();
+            ShowConnectingWindow();
+            networkManager.StartClient();
+        }
+    }
+
+    public void StopConnection()
+    {
+        networkManager.StopClient();
+        HideConnectingWindow();
+    }
+
+    private bool IsAnyNetworkServiceStarted()
+    {
+        return !networkManager.isNetworkActive;
+    }
+
+    private void SetNetworkAddress()
+    {
+        networkManager.networkAddress = networkAddressField.text;
+    }
+
+    private void ShowConnectingWindow()
+    {
+        networkManagerCanvas.transform.Find("ConnectionStatus").gameObject.SetActive(true);
+    }
+
+    private void HideConnectingWindow()
+    {
+        networkManagerCanvas.transform.Find("ConnectionStatus").gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        SwapCanvasAfterConnection();
+    }
+
+    private void SwapCanvasAfterConnection()
+    {
+        if (networkManager.isNetworkActive)
+        {
+            if (networkManager.client.isConnected)
+            {
+                HideCanvas();
+                ShowInGameCanvas();
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void HideCanvas()
+    {
+        networkManagerCanvas.gameObject.SetActive(false);
+    }
+
+    private void ShowInGameCanvas()
+    {
+        inGameCanvas.gameObject.SetActive(true);
     }
 }
